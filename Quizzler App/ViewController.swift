873268227e7e10a28fba1232456855d294b5d57d
Gospodi <gospodijazzet@gmail.com>
2022-02-02 14:18:56 +0300

    add IBOutlet for Question Label and Progress Bar

--- Quizzler App/ViewController.swift
+++ Quizzler App/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var progressBar: UIProgressView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
