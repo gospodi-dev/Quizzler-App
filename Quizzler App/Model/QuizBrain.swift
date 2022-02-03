533f8230789d8bae8fe4c004f408c40744bf51a2
Gospodi <gospodijazzet@gmail.com>
2022-02-03 11:26:31 +0300

    Добавлены вопросы на русском языке

--- Quizzler App/Model/QuizBrain.swift
+++ Quizzler App/Model/QuizBrain.swift
@@ -10,18 +10,21 @@ import Foundation
 struct QuizBrain {
     let quiz = [
         
-        Question(q: "A slug's blood is green.", a: "True"),
-        Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
-        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
-        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
-        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
-        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
-        Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
-        Question(q: "Google was originally called 'Backrub'.", a: "True"),
-        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
-        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
-        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
-        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
+        Question(q: "Первую аналоговую вычислительную машину построили в XVII в.", a: "False"),
+        Question(q: "На Земле существуют многоклеточные животные, способные пережить недельное пребывание в открытом космосе.", a: "True"),
+        Question(q: "Флаг Непала имеет треугольную форму.", a: "False"),
+        Question(q: "Иван Шишкин не рисовал медведей в сосновом лесу.", a: "True"),
+        Question(q: "Существуют сверла, способные сверлить квадратные отверстия.", a: "True"),
+        Question(q: "Французский поцелуй придумали во Франции.", a: "False"),
+        Question(q: "Существует металл, кипящий от соприкосновения с ладонью.", a: "False"),
+        Question(q: "Вода при стоке из ванны всегда закручивается против часовой стрелки в Северном полушарии и по часовой в Южном.", a: "False"),
+        Question(q: "Человек – единственное существо, способное спать на спине.", a: "False"),
+        Question(q: "Из-за инфляции доллар 1925 года соответствует примерно 100 современным долларам.", a: "False"),
+        Question(q: "Первые танки так медленно двигались по полю боя, что их мог обогнать неспешно идущий человек.", a: "True"),
+        Question(q: "Вечная мерзлота занимает почти треть территории России.", a: "False"),
+        Question(q: "Существует рыба, которая умеет передвигаться по суше и даже прыгать по деревьям.", a: "True"),
+        Question(q: "Носители языка сильбо гомера общаются свистом.", a: "True"),
+        Question(q: "Нашей Вселенной примерно 13,8 млрд лет.", a: "True")
     ]
     
     var questionNumber = 0
